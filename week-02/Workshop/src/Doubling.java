06814e8cd0416b432d2984e7d2837d59267402b9
Bea Huszár <husz.beata@gmail.com>
2018-10-30 13:30:22 +0100

    daily work update

---
+++ week-02/Workshop/src/Doubling.java
@@ -0,0 +1,11 @@
+public class Doubling {
+    public static int doubling666 (int myNumber) {
+        int newNumber = myNumber * 2;
+        return newNumber;
+    }
+
+    public static void main(String[] args) {
+        int beasnumber = 123;
+        System.out.println(doubling666(beasnumber));
+    }
+}
---
+++ week-02/Workshop/src/Sum.java
@@ -0,0 +1,10 @@
+public class Sum {
+    //  Create the usual class wrapper
+//  and main method on your own.
+
+// - Write a function called `sum` that sum all the numbers
+//   until the given parameter and returns with an integer
+
+    
+
+}

4cfa31cfd6e1326384cf11bea591efd77a3e3a24
Bea Huszár <husz.beata@gmail.com>
2018-11-05 21:45:05 +0100

    HW - HashMap practice

---
+++ practice/src/HashMaps.java
@@ -0,0 +1,29 @@
+import java.util.HashMap;
+
+public class HashMaps {
+    public static void main(String[] args) {
+        HashMap<String,String> hm = new HashMap<String, String>(); //create hashmap
+
+        hm.put("Katie", "Android, Wordpress");
+        hm.put("Magda", "Facebook");
+        hm.put("Vanessa", "Tools");
+        hm.put("Ania", "Java");
+        hm.put("Ania", "JEE"); //Java has been replaced by this value
+
+        for (String key: hm.keySet()) { //iteration
+            System.out.println(key+" : " +hm.get(key));
+        }
+        System.out.println();
+
+        if (hm.containsKey("Katie")) { //check if contains a spec Key
+            System.out.println("HashMap contains Katie yayy");
+        }
+        System.out.println();
+
+        if (hm.containsValue("Cooking")){ //check if contains a spec value
+            System.out.println("HM contains Cooking");
+        } else {
+            System.out.println("HM does not contain Cooking");
+        }
+    }
+}

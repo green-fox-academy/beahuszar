30b85ca7e4723594df1e58d9a0b834ae934db107
Bea Huszár <husz.beata@gmail.com>
2018-11-06 18:07:05 +0100

    Calculator

---
+++ week-03/day-2/src/Calculator.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+public class Calculator {
+    public static void main(String... args) {
+        // Create a simple calculator application which reads the parameters from the prompt
+        // and prints the result to the prompt.
+        // It should support the following operations,
+        // create a method named "calculate()":
+        // +, -, *, /, % and it should support two operands.
+        // The format of the expressions must be: {operation} {operand} {operand}.
+        // Examples: "+ 3 3" (the result will be 6) or "* 4 4" (the result will be 16)
+
+        // You can use the Scanner class
+        // It should work like this:
+
+        // Start the program
+        // It prints: "Please type in the expression:"
+        // Waits for the user input
+        // Print the result to the prompt
+        // Exit
+
+        Scanner myScanner = new Scanner(System.in);
+        System.out.println("Please type in the expression:");
+        String action = myScanner.nextLine();
+        System.out.println(calculate(action));
+
+    }
+    public static int calculate (String input) {
+
+        int myResult = 0;
+
+        String[] caracters = input.split(" ");
+        int operand1 = Integer.parseInt(caracters[1]);
+        int operand2 = Integer.parseInt(caracters[2]);
+
+        if (caracters[0].equals("+")) {
+            myResult = operand1 + operand2;
+        } else if (caracters[0].equals("-")) {
+            myResult = operand1 - operand2;
+        } else if (caracters[0].equals("*")) {
+            myResult = operand1 * operand2;
+        } else if (caracters[0].equals("/")) {
+            myResult = operand1 / operand2;
+        } else if (caracters[0].equals("%")) {
+            myResult = operand1 % operand2;
+        }
+
+        return myResult;
+    }
+}

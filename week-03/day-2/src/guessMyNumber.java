7ad37950cc18f964639ad5b67c4fd1e1f393ba5a
Bea Huszár <husz.beata@gmail.com>
2018-11-08 21:02:11 +0100

    [A[BguessMyNumber - all scenarios covered, minor refactoring needed

--- week-03/day-2/src/guessMyNumber.java
+++ week-03/day-2/src/guessMyNumber.java
@@ -6,32 +6,27 @@ public class guessMyNumber {
         Scanner myScanner = new Scanner(System.in);
         System.out.println("Guess a number between 1-100. You have 5 lives to use.");
         int guess = myScanner.nextInt();
-        int myNumber = 10; //(int)(Math.random() * 101);
+        int myNumber = 10; // (int)(Math.random() * 101);
         int rounds = 4;
 
-        if (myNumber == guess) {
-            System.out.println("Congratulations. You won!");
+        while (rounds > 0) {
+            if (myNumber > guess) {
+                    System.out.println("Too low. You have " + rounds + " lives left. Try again:");
+                    rounds = rounds -1;
+                    guess = myScanner.nextInt();
+            } else if (myNumber < guess) {
+                    System.out.println("Too high. You have " + rounds + " lives left. Try again:");
+                    rounds = rounds -1;
+                    guess = myScanner.nextInt();
+            } else if (myNumber == guess) {
+                System.out.println("Congratulations. You won!");
+                break;
+            }
         }
 
-
-        while (rounds > 0 && myNumber != guess) {
-            if (myNumber > guess) {
-                    System.out.println("Too low. You have " + rounds + " lives left.");
-                    rounds = rounds -1;
-                    int guess2 = myScanner.nextInt();
-            } else if (myNumber < guess) {
-                    System.out.println("Too high. You have " + rounds + " lives left.");
-                    rounds = rounds -1;
-                    int guess3 = myScanner.nextInt();
-            }
-        } System.out.println("Aaaawwww, you lost. Try again!");
-
-
-
-
-
-
-
+        if (rounds == 0) {
+            System.out.println("Aaaawwww, you lost. The correct number was: " + myNumber +" Try again!");
+        }
 
     }
 }

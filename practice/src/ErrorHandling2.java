c6536b8ad512375eaa434edd809b1affbd52a1cf
Bea Huszár <husz.beata@gmail.com>
2018-11-11 17:09:34 +0100

    Errorhandling v2

--- practice/src/ErrorHandling2.java
+++ practice/src/ErrorHandling2.java
@@ -1,4 +1,33 @@
-package PACKAGE_NAME;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 
 public class ErrorHandling2 {
+    public static void main(String[] args) {
+       /* Path path = Paths.get("practice.iml");
+        Files.isReadable(path);
+        Files.isWritable(path);
+        Files.isDirectory(path);*/
+
+        try {
+//            List<String> lines = Files.readAllLines(path);
+          /*  Files.write(path, lines);
+            Files.lines(path);*/
+            for (String line : Files.readAllLines(Paths.get("practice.iml"))) {
+                System.out.println(line.toUpperCase());
+            }
+            Files.lines(Paths.get("practice.iml"))
+
+                .map(line -> line.toUpperCase())
+                .filter(line -> !line.startsWith(" "))
+                .forEach(line -> System.out.println(line));
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 }

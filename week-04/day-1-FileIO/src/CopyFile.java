8827c0cf03f41d7ff7f7d4c74f272fe7e202ef3f
Bea Huszár <husz.beata@gmail.com>
2018-11-12 13:59:25 +0100

    CopyFile

--- week-04/day-1-FileIO/src/CopyFile.java
+++ week-04/day-1-FileIO/src/CopyFile.java
@@ -1,4 +1,33 @@
-package PACKAGE_NAME;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 
 public class CopyFile {
+    // Write a function that copies a file to an other
+// It should take the filenames as parameters
+// It should return a boolean that shows if the copy was successful
+
+    public static void main(String[] args) {
+        String myPath = "my-file.txt";
+        String myPath2 = "myfile.txt";
+
+        System.out.println(copyIt(myPath, myPath2));
+    }
+
+    public static boolean copyIt (String myPath, String myPath2) {
+        boolean returnIT = true;
+        Path newPath1 = Paths.get(myPath);
+        Path newPath2 = Paths.get(myPath2);
+
+        try {
+            List<String> nPList2 = Files.readAllLines(newPath2);
+            Files.write(newPath1, nPList2);
+            return returnIT;
+        } catch (Exception e) {
+            return !returnIT;
+        }
+
+    }
 }

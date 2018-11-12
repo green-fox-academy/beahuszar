70b464aa73661e1263c1dc38bf58a07399a460f2
Bea Huszár <husz.beata@gmail.com>
2018-11-13 00:21:10 +0100

    constructors

---
+++ practice/src/Constructors.java
@@ -0,0 +1,28 @@
+class Machine {
+    private String name;
+    private int code;
+
+    public Machine () {
+        this("Arnie",0); //it ALWAYS needs to be the first line
+        System.out.println("Constructor running");
+        name = "arnie";
+    }
+
+    public Machine (String name) {
+        System.out.println("second constructor running");
+        this.name = name;
+    }
+
+    public Machine(String name, int code){
+        System.out.println("Third constructor running");
+        this.name = name;
+        this.code = code;
+    }
+}
+public class Constructors {
+    public static void main(String[] args) {
+      Machine machine1 = new Machine();
+      Machine machine2 = new Machine("Bertie");
+      Machine machine3 = new Machine("Chalky", 7);
+    }
+}

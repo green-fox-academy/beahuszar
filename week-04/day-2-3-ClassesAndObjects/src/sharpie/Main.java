12db1248334b46c11c858012334defc0e0486025
Bea Huszár <husz.beata@gmail.com>
2018-11-13 13:33:36 +0100

    Sharpie

---
+++ week-04/day-2-3-ClassesAndObjects/src/sharpie/Main.java
@@ -0,0 +1,11 @@
+package sharpie;
+
+public class Main {
+    public static void main(String[] args) {
+        Sharpie smallBlack = new Sharpie("Black",12);
+        smallBlack.use();
+        smallBlack.use();
+        System.out.println(smallBlack.inkAmount);
+
+    }
+}

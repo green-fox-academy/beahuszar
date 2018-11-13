e4cf32ae0455767a796e717ebba325422876295c
Bea Huszár <husz.beata@gmail.com>
2018-11-13 12:15:58 +0100

    PostIt

---
+++ week-04/day-2-3-ClassesAndObjects/src/postit/Main.java
@@ -0,0 +1,9 @@
+package postit;
+
+class lMain {
+    public static void main(String[] args) {
+        PostIt postIt1 = new PostIt("orange","blue", "Idea1");
+        PostIt postIt2 = new PostIt("pink","black", "Awesome");
+        PostIt postIt3 = new PostIt("yellow","green", "Superb");
+    }
+}
---
+++ week-04/day-2-3-ClassesAndObjects/src/postit/PostIt.java
@@ -0,0 +1,14 @@
+package postit;
+
+public class PostIt {
+    String backgroundColor;
+    String text;
+    String textcolor;
+
+    PostIt(String backgroundColor, String text, String textcolor) {
+        this.backgroundColor = backgroundColor;
+        this.text = text;
+        this.textcolor = textcolor;
+    }
+}
+

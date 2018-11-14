e3b3802dee22c1a8321f8e3e41d62c0175aad6f2
Bea Huszár <husz.beata@gmail.com>
2018-11-14 12:43:11 +0100

    diceSet testing & teacherStudent completed

---
+++ week-04/day-2-3-ClassesAndObjects/src/teacherStudent/Student.java
@@ -0,0 +1,14 @@
+package teacherStudent;
+
+public class Student {
+
+  public void learn() {
+    System.out.println("lol");
+  }
+
+  public void question(Teacher teacher) {
+    Teacher teacher1 = new Teacher();
+    teacher1.answer();
+  }
+
+}
---
+++ week-04/day-2-3-ClassesAndObjects/src/teacherStudent/Teacher.java
@@ -0,0 +1,13 @@
+package teacherStudent;
+
+public class Teacher {
+
+  public void answer() {
+    System.out.println("you are smart");
+  }
+
+  public void teach(Student student) {
+    Student student1 = new Student();
+    student1.learn();
+  }
+}

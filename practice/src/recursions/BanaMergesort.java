4fa87adcd14dda559511bcc42ce1fcecd4c1c128
Bea Huszár <husz.beata@gmail.com>
2018-11-15 09:18:23 +0100

    BanaMergesort

--- practice/src/recursions/BanaMergesort.java
+++ practice/src/recursions/BanaMergesort.java
@@ -4,7 +4,7 @@ public class BanaMergesort {
   public static void main(String[] args) {
     int array[] = { 10, 8, 4, 80, 13, 1, 3, 11 };
     System.out.println("STARTING ARRAY\n");
-    printHorzArray(-1, -1, array, 49);
+//    printHorzArray(-1, -1, array, 49);
 
   }
 }

de3a98bea2101230fd48028910d00de8c4c82e33
Bea Huszár <husz.beata@gmail.com>
2018-11-15 13:21:12 +0100

    String1

---
+++ week-04/day-4-Recursion/src/String1.java
@@ -0,0 +1,16 @@
+
+public class String1 {
+  // Given a string, compute recursively (no loops) a new string where all the
+// lowercase 'x' chars have been changed to 'y' chars.
+
+  public static void main(String[] args) {
+    System.out.println(xReplace("genxogagxi"));
+  }
+
+
+  public static String xReplace (String string) {
+    if (string.contains("x")) {
+      return xReplace(string.replaceFirst("x","y"));
+    } return string;
+  }
+}

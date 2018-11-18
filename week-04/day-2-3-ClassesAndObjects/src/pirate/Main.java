fa1325e4f511afb70f39cdc33fd8480b9bde163e
Bea Huszár <husz.beata@gmail.com>
2018-11-18 11:12:37 +0100

    pirate status & clearstatus

--- week-04/day-2-3-ClassesAndObjects/src/pirate/Main.java
+++ week-04/day-2-3-ClassesAndObjects/src/pirate/Main.java
@@ -5,14 +5,9 @@ public class Main {
   private static String davyJones = "Davy Jones";
 
   public static void main(String[] args) {
-    public Pirate jack = new Pirate(2);
+    Pirate jack = new Pirate(2);
     Pirate davyJones = new Pirate(3);
     Pirate parrot = new Pirate();
-    jack.drinkSomeRum();
-    davyJones.die(davyJones);
-    davyJones.howsItGoingMate(davyJones);
-    jack.brawl(jack,davyJones);
-    parrot.parrot();
 
   }
 }
--- week-04/day-2-3-ClassesAndObjects/src/pirate/Pirate.java
+++ week-04/day-2-3-ClassesAndObjects/src/pirate/Pirate.java
@@ -1,9 +1,12 @@
 package pirate;
 
+import java.util.Scanner;
+
 public class Pirate {
   private int intoxication;
   private int drinkCounter;
   boolean death = false;
+  boolean passedout = false;
 
   public Pirate() {
 
@@ -19,7 +22,7 @@ public class Pirate {
     return intoxication ++;
   }
 
-  public void howsItGoingMate(Pirate x) {
+  public void howsItGoingMate() {
     String reply;
 
     if (death == true) {
@@ -32,27 +35,64 @@ public class Pirate {
     System.out.println(reply);
   }
 
-  public void die(Pirate x) {
+  public void die() {
     death = true;
   }
 
-  public void brawl(Pirate opp1, Pirate opp2) {
+  public void brawl(Pirate opp) {
     int chance = (int)(1 + Math.random() * 3);
 
-    if (opp1.death = true) {
-      System.out.println(opp1 + " is already dead..why would you fight him??");
-    } else if (opp2.death = true) {
-      System.out.println(opp2 + " is already dead..why would you fight him??");
+    if (this.death = true) {
+      System.out.println(this + " is already dead..why would you fight him??");
+    } else if (opp.death = true) {
+      System.out.println(opp + " is already dead..why would you fight him??");
     } else if (chance == 1) {
-      System.out.println(opp1.toString() + " won");
+      System.out.println(this + " won");
+      opp.passedout = true;
     } else if (chance == 2) {
-      System.out.println(opp2.toString() + "won");
+      System.out.println(opp  + " won");
+      this.passedout = true;
     } else {
       System.out.println("Both passed out");
+      opp.passedout = true;
+      this.passedout = true;
     }
   }
 
   public void parrot () {
     System.out.println("I am a parrot, squak, squak");
   }
+
+  public void pirateHealth() {
+    if (passedout == true) {
+      System.out.println("ZZZZZzzzzz");
+    } else if (death == true) {
+      System.out.println("kkhkhhhggggaaaargh");
+    } else howsItGoingMate();
+  }
+
+  public void clearStatus() {
+    switch (whatToDo()) {
+      case "r":
+        this.death = false;
+         System.out.println("Your pirate has been reviveth");
+         break;
+
+      case "w":
+        this.passedout = false;
+        System.out.println("Your pirate is awake now. Pour him anudder!!");
+        break;
+      }
+  }
+
+  public static String whatToDo() {
+    Scanner myScanner = new Scanner(System.in);
+
+    System.out.println("What would you like to do with your filthy pirate?");
+    System.out.println("To revive, enter [r]");
+    System.out.println("To wake him up, enter [w]");
+
+    return myScanner.nextLine();
+  }
+
 }

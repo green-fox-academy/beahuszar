394cf304f6677a3dedcd044d7bf4760f527d0f22
Bea Huszár <husz.beata@gmail.com>
2018-11-21 14:03:42 +0100

    Violin

---
+++ week-05/day-3-Abstracts-Interfaces/src/main/java/music/Violin.java
@@ -0,0 +1,12 @@
+package main.java.music;
+
+public class Violin extends StringedInstrument {
+  public Violin(String name, int numberOfStrings) {
+    super(name, numberOfStrings);
+  }
+
+  @Override
+  public void sound() {
+
+  }
+}

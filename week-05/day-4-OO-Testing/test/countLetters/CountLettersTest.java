8f5c5a151f1a75e246190a6e3866eb3c132e5100
Bea Huszár <husz.beata@gmail.com>
2018-11-22 17:59:06 +0100

    countletters completed yayyy

--- week-05/day-4-OO-Testing/test/countLetters/CountLettersTest.java
+++ week-05/day-4-OO-Testing/test/countLetters/CountLettersTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import java.util.HashMap;
 import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 

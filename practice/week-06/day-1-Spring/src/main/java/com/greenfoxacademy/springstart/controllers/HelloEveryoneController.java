8fc8e2df9156b1f7dbc0cde5be642c73a0bbe9af
Bea Huszár <husz.beata@gmail.com>
2018-12-05 17:05:06 +0100

    Say Hello to all the world (Web App)

---
+++ practice/week-06/day-1-Spring/src/main/java/com/greenfoxacademy/springstart/controllers/HelloEveryoneController.java
@@ -0,0 +1,31 @@
+package com.greenfoxacademy.springstart.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@Controller
+public class HelloEveryoneController {
+  AtomicLong atomicLong = new AtomicLong();
+
+  @RequestMapping("/web/greetall")
+  public String greeting(@RequestParam String name, Model model) {
+    long counter = atomicLong.incrementAndGet();
+    String[] hellos = {"Mirëdita", "Ahalan", "Parev", "Zdravei", "Nei Ho", "Dobrý den", "Ahoj", "Goddag", "Goede dag, Hallo", "Hello", "Saluton", "Hei", "Bonjour",
+        "Guten Tag", "Gia'sou", "Aloha", "Shalom", "Namaste", "Namaste", "Jó napot", "Halló", "Helló", "Góðan daginn", "Halo", "Aksunai", "Qanuipit", "Dia dhuit",
+        "Salve", "Ciao", "Kon-nichiwa", "An-nyong Ha-se-yo", "Salvëte", "Ni hao", "Dzien' dobry", "Olá", "Bunã ziua", "Zdravstvuyte", "Hola", "Jambo", "Hujambo", "Hej",
+        "Sa-wat-dee", "Merhaba", "Selam", "Vitayu", "Xin chào", "Hylo", "Sut Mae", "Sholem Aleychem", "Sawubona"};
+
+    model.addAttribute("name", name);
+    model.addAttribute("counter", counter);
+    model.addAttribute("hello", hellos[(int)(Math.random() * hellos.length)]);
+    model.addAttribute("fontSize", (int)(Math.random() * 100));
+    model.addAttribute("fontColor", "red");
+
+    return "allgreeting";
+  }
+
+}

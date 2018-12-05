bff2a606b59461d0a5b9b2c33a2fee66d7d154a3
Bea Huszár <husz.beata@gmail.com>
2018-12-05 14:06:51 +0100

    "refactoring with url input"

--- practice/week-06/day-1-Spring/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
+++ practice/week-06/day-1-Spring/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
@@ -1,6 +1,7 @@
 package com.greenfoxacademy.springstart.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,7 +10,7 @@ public class HelloRESTController {
 
   @RequestMapping(value="/greeting")
   @ResponseBody
-  public Greeting greeting() {
-    return new Greeting("content",12);
+  public Greeting greeting(@RequestParam String name) {
+    return new Greeting("Hello, " + name,12);
   }
 }

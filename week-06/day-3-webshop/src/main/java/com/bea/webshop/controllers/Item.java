1fe25ee856047e10323a244a9490722400cff09b
Bea Huszár <husz.beata@gmail.com>
2018-12-07 12:39:33 +0100

    adding Thymeleaf iteration

--- week-06/day-3-webshop/src/main/java/com/bea/webshop/controllers/Item.java
+++ week-06/day-3-webshop/src/main/java/com/bea/webshop/controllers/Item.java
@@ -1,20 +1,22 @@
-package com.bea.webshop;
+package com.bea.webshop.controllers;
 
+import java.util.ArrayList;
 import java.util.Currency;
+import java.util.List;
 
-public class Item extends Shop {
+public class Item {
 
   private String name;
   private String description;
-  private Currency currency;
-  private String price;
+//  private Currency currency;
+  private int price;
   private int quantityOnStock;
 
-  public Item(String name, String description, String price, int quantityOnStock) {
+  public Item(String name, String description, int price, int quantityOnStock) {
     this.name = name;
     this.description = description;
-    this.currency = Currency.getInstance("Kd");
-    this.price = price + this.currency;
+//    this.currency = Currency.getInstance("Kd");
+    this.price = price;
     this.quantityOnStock = quantityOnStock;
   }
 
@@ -25,12 +27,12 @@ public class Item extends Shop {
   public String getDescription() {
     return description;
   }
-
+/*
   public Currency getCurrency() {
     return currency;
-  }
+  }*/
 
-  public String getPrice() {
+  public int getPrice() {
     return price;
   }
 
--- week-06/day-3-webshop/src/main/java/com/bea/webshop/controllers/MyWebshop.java
+++ week-06/day-3-webshop/src/main/java/com/bea/webshop/controllers/MyWebshop.java
@@ -1,16 +1,30 @@
 package com.bea.webshop.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class MyWebshop {
+  Item item;
+  List<Item> items;
 
   public MyWebshop() {
-
+    items = new ArrayList<>();
+    items.add(new Item("Running shoes","Nike running shoes for everyday sport",1000,5));
+    items.add(new Item("Printer","Some HP printer that will print pages",3000,2));
+    items.add(new Item("Coca Cola","0.5l standard coke",25,0));
+    items.add(new Item("Wokin","Chicken with fried rice and WOKIN sauce",119,100));
+    items.add(new Item("T-shirt","Blue with a corgi on a bike",300,1));
   }
+
+
   @RequestMapping(value="/webshop")
-  public String greeting() {
+  public String greeting(Model model) {
+    model.addAttribute("items", items);
     return "webshop";
   }
 }

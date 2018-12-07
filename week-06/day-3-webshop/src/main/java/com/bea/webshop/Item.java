bbefcf74453577614590fe1720f7483b4dff22cb
Bea Huszár <husz.beata@gmail.com>
2018-12-07 11:54:05 +0100

    creating Items Arraylist

--- week-06/day-3-webshop/src/main/java/com/bea/webshop/Item.java
+++ week-06/day-3-webshop/src/main/java/com/bea/webshop/Item.java
@@ -1,4 +1,40 @@
 package com.bea.webshop;
 
-public class Item {
+import java.util.Currency;
+
+public class Item extends Shop {
+
+  private String name;
+  private String description;
+  private Currency currency;
+  private String price;
+  private int quantityOnStock;
+
+  public Item(String name, String description, String price, int quantityOnStock) {
+    this.name = name;
+    this.description = description;
+    this.currency = Currency.getInstance("Kd");
+    this.price = price + this.currency;
+    this.quantityOnStock = quantityOnStock;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public Currency getCurrency() {
+    return currency;
+  }
+
+  public String getPrice() {
+    return price;
+  }
+
+  public int getQuantityOnStock() {
+    return quantityOnStock;
+  }
 }

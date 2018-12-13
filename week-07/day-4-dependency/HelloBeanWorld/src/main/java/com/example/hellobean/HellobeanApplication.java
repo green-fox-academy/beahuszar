6cffe62386c53c6573b4d98c5f76e6b98b363a5b
Bea Huszár <husz.beata@gmail.com>
2018-12-13 12:23:01 +0100

    hellobean

---
+++ week-07/day-4-dependency/HelloBeanWorld/src/main/java/com/example/hellobean/HellobeanApplication.java
@@ -0,0 +1,23 @@
+package com.example.hellobean;
+
+import com.example.hellobean.service.Printer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellobeanApplication implements CommandLineRunner {
+  @Autowired
+  Printer printer;
+
+  public static void main(String[] args) {
+    SpringApplication.run(HellobeanApplication.class, args);
+  }
+
+  @Override
+  public void run(String... args) throws Exception {
+    printer.log("mukoggy");
+  }
+}
+
---
+++ week-07/day-4-dependency/HelloBeanWorld/src/test/java/com/example/hellobean/HellobeanApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.hellobean;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HellobeanApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
+

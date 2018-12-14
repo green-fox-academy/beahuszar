03128eb49806d97d4cc06da5ffecb33142581f5c
Bea Huszár <husz.beata@gmail.com>
2018-12-14 12:41:15 +0100

    restoring destruction

---
+++ week-07/day-5-tamagotchi/src/main/java/com/greenfoxacademy/tamagotchi/controller/TamagotchiController.java
@@ -0,0 +1,26 @@
+package com.greenfoxacademy.tamagotchi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class TamagotchiController {
+
+  @GetMapping("/")
+  public String index() {
+    return "index";
+  }
+
+  @GetMapping("/login")
+  public String login() {
+    return "login";
+  }
+
+  @PostMapping("/login")
+  public String login(String nickname) {
+    return "redirect:/";
+  }
+
+}

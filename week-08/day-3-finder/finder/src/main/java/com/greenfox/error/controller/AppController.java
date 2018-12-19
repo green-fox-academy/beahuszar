229b052971c562b1920d80785e9d5f2e02a4f22a
Bea Huszár <husz.beata@gmail.com>
2018-12-19 13:01:24 +0100

    Finding and fixing bugs

---
+++ week-08/day-3-finder/finder/src/main/java/com/greenfox/error/controller/AppController.java
@@ -0,0 +1,40 @@
+package com.greenfox.error.controller;
+
+import com.greenfox.error.service.UserService;
+import com.greenfox.error.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by aze on 25/10/17.
+ */
+@Controller
+@RequestMapping("/app")
+public class AppController {
+
+
+  UserService service;
+
+  @Autowired
+  public AppController(UserService service) {
+    this.service = service;
+  }
+
+  @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("new_user", new User());
+        model.addAttribute("yolo", service.getAll());
+        return "main";
+    }
+
+    @PostMapping("/")
+    public String create(@ModelAttribute User user) {
+        service.save(user);
+        return "redirect:/app/";
+    }
+}

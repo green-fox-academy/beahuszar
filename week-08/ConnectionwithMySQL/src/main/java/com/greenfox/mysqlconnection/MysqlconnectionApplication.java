476591ffc71ca5d66464a2b16b5a32739c71c3b7
Bea Huszár <husz.beata@gmail.com>
2018-12-22 16:09:03 +0100

    removing hard coded todos

--- week-08/ConnectionwithMySQL/src/main/java/com/greenfox/mysqlconnection/MysqlconnectionApplication.java
+++ week-08/ConnectionwithMySQL/src/main/java/com/greenfox/mysqlconnection/MysqlconnectionApplication.java
@@ -10,30 +10,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MysqlconnectionApplication implements CommandLineRunner {
-  TodoService service;
-  AssigneeService assigneeService;
-
-  @Autowired
-  public MysqlconnectionApplication(TodoService service, AssigneeService assigneeService) {
-    this.service = service;
-    this.assigneeService = assigneeService;
-  }
-
-
+public class MysqlconnectionApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(MysqlconnectionApplication.class, args);
-  }
-
-
-  @Override
-  public void run(String... args) throws Exception {
-    service.addTodo(new Todo("wear ugly sweater on the Xmas Party",true, false));
-    service.addTodo(new Todo("petting the class' dog",true, true));
-    assigneeService.saveAssignees(new Assignee("Blanka", "Hooz", "blanka.hooz@greenfox.com"));
-    assigneeService.saveAssignees(new Assignee("Gabor", "Wermer", "gabor.wermer@greenfox.com"));
-    assigneeService.saveAssignees(new Assignee("Istvan", "Karasz", "istvan.karasz@greenfox.com"));
   }
 }
 

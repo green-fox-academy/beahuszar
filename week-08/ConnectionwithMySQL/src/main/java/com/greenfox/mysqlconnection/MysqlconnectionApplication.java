e092f4d0abbb4391221117f5e90bb25adbf68c35
Bea Huszár <husz.beata@gmail.com>
2018-12-24 22:31:28 +0100

    refactor

--- week-08/ConnectionwithMySQL/src/main/java/com/greenfox/mysqlconnection/MysqlconnectionApplication.java
+++ week-08/ConnectionwithMySQL/src/main/java/com/greenfox/mysqlconnection/MysqlconnectionApplication.java
@@ -1,11 +1,5 @@
 package com.greenfox.mysqlconnection;
 
-import com.greenfox.mysqlconnection.model.Assignee;
-import com.greenfox.mysqlconnection.model.Todo;
-import com.greenfox.mysqlconnection.service.AssigneeService;
-import com.greenfox.mysqlconnection.service.TodoService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 

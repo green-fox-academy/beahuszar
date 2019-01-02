32fef768b2e7e55f6f31d3fc7ebcba1b444e21ec
Bea Huszár <husz.beata@gmail.com>
2019-01-02 16:53:31 +0100

    add: CRUD service & repository

---
+++ week-08/reddit/src/main/java/com/greenfox/redditapp/repository/RedditRepository.java
@@ -0,0 +1,8 @@
+package com.greenfox.redditapp.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RedditRepository extends CrudRepository<RedditItem, Float> {
+}
---
+++ week-08/reddit/src/main/java/com/greenfox/redditapp/service/RedditService.java
@@ -0,0 +1,18 @@
+package com.greenfox.redditapp.service;
+
+import com.greenfox.redditapp.repository.RedditRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RedditService {
+
+  private RedditRepository repository;
+
+  @Autowired
+  public RedditService(RedditRepository repository) {
+    this.repository = repository;
+  }
+
+  
+}

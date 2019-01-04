6d90a4933ca62d4918a22721b2d8fe82c0044c8d
Bea Huszár <husz.beata@gmail.com>
2019-01-04 13:29:00 +0100

    upd: going with tricklist instead

--- week-07/day-5-tamagotchi/src/main/java/com/greenfoxacademy/tamagotchi/repository/Pet.java
+++ week-07/day-5-tamagotchi/src/main/java/com/greenfoxacademy/tamagotchi/repository/Pet.java
@@ -1,6 +1,8 @@
 package com.greenfoxacademy.tamagotchi.repository;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "pets")
@@ -10,12 +12,17 @@ public class Pet {
   @GeneratedValue(strategy = GenerationType.AUTO)
   private long id;
   private String name;
+  private List<Trick> trickList;
+  private String food;
+  private String drink;
 
   public Pet() {
+    this.trickList = new ArrayList<>();
   }
 
   public Pet(String name) {
     this.name = name;
+    this.trickList = new ArrayList<>();
   }
 
   public String getName() {
@@ -32,5 +39,29 @@ public class Pet {
 
   public void setId(long id) {
     this.id = id;
+  }
+
+  public List<Trick> getTrickList() {
+    return trickList;
+  }
+
+  public void setTrickList(List<Trick> trickList) {
+    this.trickList = trickList;
+  }
+
+  public String getFood() {
+    return food;
+  }
+
+  public void setFood(String food) {
+    this.food = food;
+  }
+
+  public String getDrink() {
+    return drink;
+  }
+
+  public void setDrink(String drink) {
+    this.drink = drink;
   }
 }

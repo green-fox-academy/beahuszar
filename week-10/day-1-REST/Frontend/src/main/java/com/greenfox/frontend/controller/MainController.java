68c64a8fa5eb0ee5bc038c485e2038141d65cf35
Bea Huszár <husz.beata@gmail.com>
2019-01-07 13:03:50 +0100

    add: index and frontend

--- week-10/day-1-REST/Frontend/src/main/java/com/greenfox/frontend/controller/MainController.java
+++ week-10/day-1-REST/Frontend/src/main/java/com/greenfox/frontend/controller/MainController.java
@@ -1,4 +1,13 @@
 package com.greenfox.frontend.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class MainController {
+
+  @GetMapping("/")
+  public String main() {
+    return "index";
+  }
 }

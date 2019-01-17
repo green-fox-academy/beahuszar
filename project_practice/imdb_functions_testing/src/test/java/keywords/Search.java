fc551c22a73199fabbae1cb7f1ee9aa862e8a41a
Bea Huszár <husz.beata@gmail.com>
2019-01-17 15:13:52 +0100

    initial commit

---
+++ project_practice/imdb_functions_testing/src/test/java/keywords/Search.java
@@ -0,0 +1,29 @@
+package keywords;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+public class Search {
+
+  public Search() {
+    System.setProperty("webdriver.chrome.driver", "lib/chromedriver.exe");
+  }
+
+  public static void searchMovie(String movieTitle, WebDriver driver) {
+    driver.findElement(By.id("navbar-query")).sendKeys(movieTitle, Keys.ENTER);
+  }
+
+  public static String getSearchResult(WebDriver driver) {
+    WebDriverWait wait = new WebDriverWait(driver, 10);
+    WebElement searchResult = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"main\"]/div/h1")));
+    return searchResult.getText();
+  }
+
+
+}

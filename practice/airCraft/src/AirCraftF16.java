37da160fc3b2c174c19d676156b141c12457d77e
Bea Huszár <husz.beata@gmail.com>
2019-01-17 15:30:50 +0100

    refactor

--- practice/airCraft/src/AirCraftF16.java
+++
@@ -1,7 +0,0 @@
-public class AirCraftF16 extends AirCraft {
-
-  public AirCraftF16() {
-    super(8, 30, 0);
-  }
-
-}
--- practice/airCraft/src/AirCraftF35.java
+++
@@ -1,7 +0,0 @@
-public class AirCraftF35 extends AirCraft {
-
-  public AirCraftF35() {
-    super(12, 50, 0);
-  }
-
-}

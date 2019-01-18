4139f09724a8fd6a4aecc80100963300ecf31ec2
Bea Huszár <husz.beata@gmail.com>
2019-01-18 12:11:06 +0100

    feat(Login): add login keywords & test

---
+++ project_practice/imdbSufaceTest/src/test/java/logintest/LoginTest.java
@@ -0,0 +1,31 @@
+package logintest;
+
+import keywords.Login;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.Assert.assertEquals;
+
+public class LoginTest {
+  private WebDriver driver;
+
+  @Before
+  public void setUp() {
+    System.setProperty("webdriver.chrome.driver", "lib/chromedriver.exe");
+    driver = new ChromeDriver();
+  }
+
+  /*@After
+  public void tearDown() {
+    driver.quit();
+  }*/
+
+  @Test
+  public void loginToPage() {
+    Login.imdbLogin("husz.beata@gmail.com", "imdbPsw2019", "https://www.imdb.com/", driver);
+
+    assertEquals("Beáta", Login.userName(driver));
+  }
+}

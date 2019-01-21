27da558474a0c90f7439df4d0a96c8e29ec0eba2
Bea Huszár <husz.beata@gmail.com>
2019-01-21 11:36:45 +0100

    feat(watchIssue): add open issues method[D[D[D[D[D[D[D[D[D[D[D[D[list

---
+++ project_practice/Jira/src/test/java/keywords/WatchIssue.java
@@ -0,0 +1,22 @@
+package keywords;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class WatchIssue {
+  private static WebElement myOpenIssues;
+  private static WebDriverWait wait;
+
+  public WatchIssue() {
+    System.setProperty("webdriver.chrome.driver", "lib/chromedriver.exe");
+  }
+
+  public static void openFirstIssueFromIssuesMenu(WebDriver driver) {
+    wait = new WebDriverWait(driver, 10);
+    myOpenIssues = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("filter_lnk_my_lnk")));
+    myOpenIssues.click();
+  }
+}

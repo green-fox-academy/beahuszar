2f141fbe8f7da45cc8431af54982748267345ac0
Bea Huszár <husz.beata@gmail.com>
2019-05-19 11:46:07 +0200

    upd: fix errors

--- week-04/day-4-Recursion/src/NumberAdder.java
+++ week-04/day-4-Recursion/src/NumberAdder.java
@@ -2,16 +2,16 @@ public class NumberAdder {
   // Write a recursive function that takes one parameter: n and adds numbers from 1 to n.
 
   public static void main(String[] args) {
-    numberAdder(7);
+    System.out.println(numberAdder(5));
   }
 
-  public static void numberAdder (int n) {
+  public static int numberAdder (int n) {
 
-    if (n == 0) {
-      return;
+    if (n == 1) {
+      return 1;
+    } else {
+      int result = numberAdder(n - 1) + n;
+      return result;
     }
-    numberAdder(n - 1);
-    System.out.println(n);
-
   }
 }
